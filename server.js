b9f411e0f8488096adf3811a0421ce55a4275131
Esteban Mendez <edmendezn@gmail.com>
2021-03-05 10:44:34 -0500

    Added/edited comments

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// HTML GET requests
+// HTML GET request
 // Code below handles when users "visit" a page.
 // The user is shown an HTML page of content
 app.get("/notes", (req, res) => {
@@ -28,43 +28,58 @@ app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
+// Code below handles when the user submits data to the server
 // POST route will allow the user to add a new note and save it
+// The saved note will will save in our database (db.json)
 app.post("/api/notes", (req, res) => {
   let newNote = req.body;
+  // Use fs to read the db.json file and later parse this data so it can be modified
   fs.readFile("./db/db.json", function (err, data) {
     if (err) {
       throw err;
     } else {
+      // Parse data so we can push a new note into it
       let notes = JSON.parse(data);
       notes.push(newNote);
       // Each item in the JSON data is given an index number
-      // i + 1 will start the id's at number 1 ...
+      // Index numbers will begin at 1 since we are adding i + 1 for each item
       notes.forEach((item, i) => {
         item.id = i + 1;
       });
-
+      // Use fs to write (send) the new data
+      // Data has to be converted to a string
       fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
         if (err) throw err;
+        // send response with new data
+        res.json(newNote);
       });
     }
   });
-  res.json(newNote);
 });
 
-// DELETE route
+// DELETE route will allow the user to delete a note
+// Will delete the specified data from the database
 app.delete("/api/notes/:id", (req, res) => {
   let deleteId = req.params.id;
+  // Use fs to read the db.json file and later parse this data so it can be modified
   fs.readFile("./db/db.json", function (err, data) {
     if (err) {
       throw err;
     } else {
+      // Parse data then...
       let notes = JSON.parse(data);
       notes.forEach((note) => {
+        // Compare id's then...
         if (deleteId == note.id) {
+          // Grab the index to be deleted ...
           let currentIndex = notes.indexOf(note);
+          // and delete it.
           notes.splice(currentIndex, 1);
+          // Use fs to write (send) the new data
+          // Data has to be converted to a string
           fs.writeFile("./db/db.json", JSON.stringify(notes), function (err) {
             if (err) throw err;
+            // send response with new data
             res.json(notes);
           });
         }
@@ -78,6 +93,7 @@ app.delete("/api/notes/:id", (req, res) => {
 // Home route
 // Code below handles when users "visit" a page.
 // The user is shown an HTML page of content
+// If no matching route is found then default to home
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
